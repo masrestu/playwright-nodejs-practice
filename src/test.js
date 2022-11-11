1d0887aadec93b3729fcf242e0bf10328256a966
masrestu <n.rizkiawan@gmail.com>
2022-11-11 18:27:49 +0700

    Practice Automation - Simple Datepicker,  Dropdown Datepicker

--- src/test.js
+++ src/test.js
@@ -1,35 +1,105 @@
-const playwright = require('playwright');
+const playwright = require('playwright')
 
 const main = async () => {
     const browser = await playwright.chromium.launch({
-        headless: true
-    });
-
-    const page = await browser.newPage();
-    await page.goto('https://www.globalsqa.com/demo-site/accordion-and-tabs/');
-
-    const elements = await page.$eval('ul.resp-tabs-list', async (ul) => {
-        const data = [];
-        const tabs = ul.getElementsByTagName('li')
-
-        const accordionBody = ul.nextElementSibling
-        const h2s = accordionBody.querySelectorAll('h2')
-
-        for (const tab of tabs) {
-            const currentTab = tab.textContent
-            tab.click()
-
-            for (const h2 of h2s) {
-                if (h2.textContent === tab.textContent) {
-                    const accordionList = h2.nextElementSibling.querySelector('h3')
-                    data.push(accordionList)
-                }
-            }
-        }
-        return data
+        headless: false
     })
-    await browser.close();
-    console.log(elements);
+
+    const page = await browser.newPage()
+    await page.goto('https://www.globalsqa.com/demo-site/datepicker/')
+
+    const tabList = page.getByRole(`tab`)
+    const tabCount = await tabList.count()
+
+    for (let tabIndex = 0; tabIndex < tabCount; tabIndex++) {
+        const tab = tabList.nth(tabIndex)
+        tabTitle = await tab.textContent()
+        await tab.click()
+
+        const iframe = page.frameLocator(`//div[@rel-title='${tabTitle}']//iframe[contains(@class, 'demo-frame')]`)
+
+        if (tabIndex == 1) {
+            const datepicker = iframe.locator(`//*[@id='datepicker']`)
+            await datepicker.waitFor({ state: 'attached' })
+            await datepicker.click()
+
+            const expectedYear = 1994
+            const expectedMonth = 'Jan'
+            const expectedDay = '31'
+
+            // selecting year
+            const selectedYear = iframe.locator(`//select[contains(@class, 'ui-datepicker-year')]`)
+            await selectedYear.waitFor({ state: `attached` })
+
+            const optionYear = selectedYear.locator(`//option`)
+            console.log(await optionYear.count());
+            let minYear = optionYear.first()
+            let maxYear = optionYear.last()
+            let minYearValue = parseInt(await minYear.textContent())
+            let maxYearValue = parseInt(await maxYear.textContent())
+
+            let position = (expectedYear < minYearValue) ? 'below' : 'current'
+            position = (expectedYear > maxYearValue) ? 'above' : position
+            console.log(position, expectedYear, minYearValue, maxYearValue, typeof expectedYear, typeof minYearValue, typeof maxYearValue);
+            while (position !== 'current') {
+                if (position === 'below') {
+                    minYearValue = parseInt(await minYear.textContent())
+                    await selectedYear.selectOption(minYearValue.toString())
+                } else {
+                    maxYearValue = parseInt(await maxYear.textContent())
+                    await selectedYear.selectOption(maxYearValue.toString())
+                }
+
+                position = (expectedYear < minYearValue) ? 'below' : 'current'
+                position = (expectedYear > maxYearValue) ? 'above' : position
+                console.log(position, expectedYear, minYearValue, maxYearValue, typeof expectedYear, typeof minYearValue, typeof maxYearValue);
+            }
+
+            await selectedYear.selectOption(expectedYear.toString())
+
+            // selecting month
+            const selectedMonth = iframe.locator(`//select[contains(@class, 'ui-datepicker-month')]`)
+            await selectedMonth.waitFor({ state: `attached` })
+            await selectedMonth.selectOption({ label: expectedMonth })
+
+            const calendar = iframe.locator(`//a[@class='ui-state-default']`).filter({
+                hasText: `'${expectedDay}'`
+            })
+            await calendar.click()
+        }
+
+        if (tabIndex == 0) {
+            const datepicker = iframe.locator(`//*[@id='datepicker']`)
+            await datepicker.waitFor({ state: 'attached' })
+            await datepicker.click()
+
+            const selectedYear = iframe.locator(`//*[@class='ui-datepicker-year']`)
+            const selectedMonth = iframe.locator(`//*[@class='ui-datepicker-month']`)
+            await selectedYear.waitFor({ state: `attached` })
+
+            const prevBtn = iframe.locator(`//a[@title='Prev']`)
+            let currentYear = await selectedYear.textContent()
+            let currentMonth = await selectedMonth.textContent()
+
+            const expectedYear = '2021'
+            const expectedMonth = 'June'
+            const expectedDay = '15'
+
+            while (!(expectedYear === currentYear && expectedMonth === currentMonth)) {
+                await prevBtn.click()
+                currentYear = await selectedYear.textContent()
+                currentMonth = await selectedMonth.textContent()
+            }
+
+            const calendar = iframe.locator(`//a[@class='ui-state-default']`).filter({
+                hasText: `'${expectedDay}'`
+            })
+            await calendar.click()
+        }
+        await page.waitForTimeout(2000)
+    }
+
+    await browser.close()
 }
 
-main();
+main()
